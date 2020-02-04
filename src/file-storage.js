1bc490734d0b0bf65c445000b7943af598156a62
Zoltan Kis <zoltan.kis@intel.com>
2020-02-04 10:13:05 +0200

    Debug uploads
    Signed-off-by: Zoltan Kis <zoltan.kis@intel.com>

--- src/file-storage.js
+++ src/file-storage.js
@@ -71,18 +71,18 @@ export class FileStorage extends EventTarget {
 
   async chooseTokens() {
     const entries = await this.chooseFileSystemEntriesFlat({type: 'openDirectory'});
-    const re = new RegExp('^token_[0-9_]+\.json$');
+    //const re = new RegExp('^token_[0-9_]+\.json$');
 
     const db = await this.dbPromise;
     for await (const entry of entries) {
-      if (re.test(entry.name)) {
+      // if (re.test(entry.name)) {
         const file = entry;
         const text = await file.text();
         const json = JSON.parse(text);
         if (json.token) {
           await db.put('tokens', json);
         }
-      }
+      //}
     }
   }
 
